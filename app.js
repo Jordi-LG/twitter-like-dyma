404e2fc7a311daaf79e6161167a122d4def15203
Jordi-LG <jordy.leguet@gmail.com>
2020-10-15 16:13:00 +0200

    feat: connect views folder

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
+
+// app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
 
 app.listen(port);
