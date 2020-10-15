c8f6a8faff6586dd56481377dca9a310832e1173
Jordi-LG <jordy.leguet@gmail.com>
2020-10-15 16:29:51 +0200

    feat: define express middleware + morgan

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
+const morgan = require("morgan");
+// récupération d'index.js dans le folder routes
+const index = require("./routes");
 
 // app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "pug");
+
+// ==== MIDDLEWARE EXPRESS
+// Je récupère les fichiers que je retournerai à l'utilisateur via le middleware static
+app.use(express.static(path.join(__dirname, "public")));
+
+// Récuperation de la data via la methode POST
+app.use(express.json());
+// Récuperation de la data mais autre format que JSON
+// + object complexe avec true
+app.use(express.urlencoded({ extended: true }));
+// middleware logger
+app.use(morgan("short"));
+
+app.use(index);
 
 app.listen(port);
