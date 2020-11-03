ac287b754f3043f4b884fad30adc968656dc8998
Jordi-LG <jordy.leguet@gmail.com>
2020-11-03 08:03:44 +0100

    feat: create an user with hashed password in DB

--- database/models/user.model.js
+++ database/models/user.model.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 // Schema pour les documents tweets
 const userSchema = schema({
-  content: {
-    username: { type: String, required: true },
-    local: {
-      email: { type: String, required: true },
-      password: { type: String, required: true },
-    },
+  username: { type: String, required: true },
+  local: {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
   },
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hash(password, 12);
+};
 
 // On associe le schema du document à la collection tweets
 const User = mongoose.model("user", userSchema);
