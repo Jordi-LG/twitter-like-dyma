ae3795af268e96214169f7c9a0ae483b8c3d089b
Jordi-LG <jordy.leguet@gmail.com>
2020-11-05 12:48:39 +0100

    fix: fix in prod img upload

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -12,7 +12,7 @@ const multer = require("multer");
 const upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, path.join(__dirname, "../public/img/avatars"));
+      cb(null, path.resolve(__dirname, "../public/img/avatars"));
     },
     filename: (req, file, cb) => {
       cb(null, `${Date.now()}-${file.originalname}`);
